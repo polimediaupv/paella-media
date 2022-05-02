da146238e777604711d8789501878b6b1978de73
Fernando Serrano Carpena <ferserc1@gmail.com>
2022-05-02 14:19:43 +0200

    media user tracking plugin implemented

--- src/plugins/es.upv.paella.media.userTrackingDataPlugin.js
+++ src/plugins/es.upv.paella.media.userTrackingDataPlugin.js
@@ -7,17 +7,25 @@ export default class MediaUserTrackingDataPlugin extends DataPlugin {
     }
 
     async write(context, { id }, data) {
-        console.log(`id: ${ id }`, context, data);
-        // https://media.upv.es/rest/plugins/eventsaver/userevent
-        /*
-        {
-            "date": "2022-04-29T10:36:03.463Z",
-            "videoId": "be0c7738-039d-9445-8237-8b85f37cd303",
-            "playing": false,
-            "time": null,
-            "event": "paella:loadComplete",
-            "params": {}
+        const playing = !(await this.player.paused());
+        const date = new Date();
+        const userEventData = {
+            date: date.toISOString(),
+            videoId: id,
+            playing,
+            time: date.toLocaleTimeString(),
+            event: data.event,
+            params: {
+                plugin: data.params?.plugin?.name || ""
+            }
         }
-        */
+        const result = await fetch('https://media.upv.es/rest/plugins/eventsaver/userevent', {
+            method: 'POST',
+            body: JSON.stringify(userEventData)
+        });
+        if (!result.ok) {
+            console.warn("Error sending user data");
+            console.warn(result);
+        }
     }
 }
