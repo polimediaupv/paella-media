bafa113357834dd2e584132b9edbd7f507529223
Fernando Serrano Carpena <ferserc1@gmail.com>
2022-05-10 14:08:48 +0200

    choice question added to quiz plugin

--- src/plugins/es.upv.paella.media.quizEventPlugin.js
+++ src/plugins/es.upv.paella.media.quizEventPlugin.js
@@ -1,4 +1,76 @@
-import { Events, EventLogPlugin } from 'paella-core';
+import { Events, EventLogPlugin, PopUp, createElementWithHtmlText } from 'paella-core';
+
+import '../css/QuizEventPlugin.css';
+
+function getQuestionElement(question) {
+    let elem = null;
+    switch (question.type) {
+    case 'choice-question':
+        elem = createElementWithHtmlText(`<div></div>`);
+        createElementWithHtmlText(`${question.question}`,elem);
+        question.responses.forEach((response,i) => {
+            const id = `choice_${i}`;
+            createElementWithHtmlText(`
+                <div>
+                    <input type="radio" id="${id}" name="choiceQuestion"/>
+                    <label for="${id}">${response}</label>
+                </div>
+            `, elem);
+        })
+    }
+    const buttons = createElementWithHtmlText(`
+        <div>
+            <button class="ok-button">Validate</button>
+            <button class="quiz-next-button" style="display: none;">Next</button>
+        </div>`, elem);
+    const nextButton = buttons.getElementsByClassName('quiz-next-button')[0];
+    const okButton = buttons.getElementsByClassName('ok-button')[0];
+    okButton.addEventListener('click', evt => {
+        alert("Validate");
+        okButton.style.display = "none";
+        nextButton.style.display = "";
+    });
+    return elem;
+}
+
+function getQuestionContainer(question, doneCallback) {
+    const questionContainer = createElementWithHtmlText(`
+        <div class="question-container">
+        </div>
+    `);
+    questionContainer.addEventListener('click', evt => {
+        evt.stopPropagation();
+    });
+
+    const quizzes = question.quizzes.map(q => {
+        const elem = getQuestionElement(q);
+
+        const nextButton = elem.getElementsByClassName('quiz-next-button')[0];
+        nextButton.addEventListener('click', evt => {
+            evt.stopPropagation();
+            questionContainer._data.nextQuestion();
+        });
+        return elem;
+    });
+
+    questionContainer._data = {
+        quizzes,
+        current: 0,
+        nextQuestion() {
+            const elem = this.quizzes[this.current++];
+            if (elem) {
+                questionContainer.innerHTML = "";
+                questionContainer.appendChild(elem);
+            }
+            else {
+                doneCallback();
+            }
+        }
+    }
+
+    questionContainer._data.nextQuestion();
+    return questionContainer
+}
 
 export default class QuizEventPlugin extends EventLogPlugin {
     async load() {
@@ -13,6 +85,30 @@ export default class QuizEventPlugin extends EventLogPlugin {
             Events.PLAY,
             Events.SEEK
         ];
+    }
+
+    showQuestion(question) {
+        if (this._questionModal) {
+            this.hideQuestion();
+        }
+
+        this._questionModal = new PopUp(this.player, document.body, null, null, true);
+        const questionContainer = getQuestionContainer(question, () => {
+            this.hideQuestion();
+        });
+        this._questionModal.setContent(questionContainer);
+        this._questionModal.contentElement.classList.add('question-modal');
+        this._questionModal.show();
+    }
+
+    hideQuestion() {
+        if (this._questionModal) {
+            this._questionModal.hide();
+            this._questionModal = null;
+            this._playAllowed = true;
+            this._skipTime = Math.round(this._currentTime);
+            this.player.play();
+        }
     }
 
     async onEvent(event, params) {
@@ -40,12 +136,15 @@ export default class QuizEventPlugin extends EventLogPlugin {
 
     async timeUpdate(time) {
         const roundedTime = Math.round(time);
-        const question = this._quiz?.quizTimes?.find(q => q.time === roundedTime);
+        const question = this._quiz?.quizTimes?.find(q => q.time === roundedTime && q.time !== this._skipTime);
         if (question) {
-            console.log(question);
             await this.player.pause();
             this._playAllowed = false;
             this._currentTime = await this.player.videoContainer.currentTime();
+            this.showQuestion(question);
+        }
+        if (this._skipTime > 0 && this._skipTime !== roundedTime) {
+            this._skipTime = null;
         }
     }
 }
